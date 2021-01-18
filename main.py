bd8d3b007e7c6c34e469b785b3aecb0ed0d61d48
Anastasiia Romanova <anastasia.romashka@gmail.com>
2021-01-18 23:08:19 +0100

    version edit + beautifying

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import xml.etree.ElementTree as ET
 import time
 
-# f = open("ids.txt", "x")
+f = open("ids.txt", "x")
 start_time = time.time()
 for i in range(1, 4622):
     key = '6C78A9C3-VB95-2721-9342-080027F47A94'
@@ -16,11 +16,10 @@ for i in range(1, 4622):
         print("--- %s seconds ---" % round(time.time() - start_time))
         # print(child.tag)
         if child.tag == 'wsrp' and child.attrib['status'] == 'ok':
-
             for child3 in root.iter('*'):
                 if child3.tag == 'name' and ('Filmora' in child3.text):
                     for child2 in root.iter('*'):
-                        if child2.tag == 'version' and str(child2.text)[0:1] == '9':
+                        if child2.tag == 'version' and str(child2.text)[0:1] == '8':
                             print('Filmora found!', 'id =', i)
                             f = open("ids.txt", "a")
                             f.write(str(i) + "\n")
