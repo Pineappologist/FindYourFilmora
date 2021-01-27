48b831f5fbe771fed8fce9e603c49ba90808e078
Anastasiia Romanova <anastasia.romashka@gmail.com>
2021-01-27 18:12:29 +0100

    slightly optimized, not bug checked

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ import requests
 import xml.etree.ElementTree as ET
 import time
 
-info_ok = False
-product_ok = False
-version_ok = False
+
 
 # f = open("ids_9(3).txt", "x")
 start_time = time.time()
-for i in range(717, 4622):
+for i in range(1, 4622):
     key = '6C78A9C3-VB95-2721-9342-080027F47A94'
     not_i = 4222
     r = requests.get(f'http://platform.wondershare.com/rest/v2/downloader/runtime/?client_sign={key}&product_id={i}')
     root = ET.fromstring(r.content)
-
+    info_ok = False
+    product_ok = False
+    version_ok = False
     for child in root.iter('*'):
         print("--- %s seconds ---" % round(time.time() - start_time))
         if child.tag == 'wsrp' and child.attrib['status'] == 'ok':
@@ -31,9 +31,10 @@ for i in range(717, 4622):
             f = open("ids_9(3).txt", "a")
             f.write(str(i) + "\n")
             f.close()
-            info_ok = False
-            product_ok = False
-            version_ok = False
+            break
+
+
+
 
         # print("--- %s seconds ---" % round(time.time() - start_time))
         # print(child.tag)
