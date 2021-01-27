cef1ec65bff205aafe6101244dab433d2c2a999f
Anastasiia Romanova <anastasia.romashka@gmail.com>
2021-01-27 18:24:31 +0100

    filled ids search

--- main.py
+++ main.py
@@ -5,36 +5,28 @@ import requests
 import xml.etree.ElementTree as ET
 import time
 
-
-
-# f = open("ids_9(3).txt", "x")
+f = open("ex_ids.txt", "x")
 start_time = time.time()
-for i in range(1, 4622):
+for i in range(4622, 10000):
     key = '6C78A9C3-VB95-2721-9342-080027F47A94'
     not_i = 4222
     r = requests.get(f'http://platform.wondershare.com/rest/v2/downloader/runtime/?client_sign={key}&product_id={i}')
     root = ET.fromstring(r.content)
     info_ok = False
     product_ok = False
-    version_ok = False
     for child in root.iter('*'):
         print("--- %s seconds ---" % round(time.time() - start_time))
         if child.tag == 'wsrp' and child.attrib['status'] == 'ok':
             info_ok = True
-        if child.tag == 'version' and str(child.text)[0:1] == '9':
-            version_ok = True
         if child.tag == 'name' and ('Filmora' in child.text):
             product_ok = True
 
-        if info_ok and product_ok and version_ok:
-            print(i, 'is the right one')
-            f = open("ids_9(3).txt", "a")
+        if info_ok and product_ok:
+            print(i, 'exists')
+            f = open("ex_ids.txt", "a")
             f.write(str(i) + "\n")
             f.close()
             break
-
-
-
 
         # print("--- %s seconds ---" % round(time.time() - start_time))
         # print(child.tag)
